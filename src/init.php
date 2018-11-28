6933219f08a57ed92a71c7edfe27fc83ea33d7e6
KEVIN BAZIRA <kvnbzr@gmail.com>
2018-11-28 14:22:52 +0300

    Added: wp-editor dependency to editor_assets

--- src/init.php
+++ src/init.php
@@ -21,7 +21,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 function algori_pdf_viewer_cgb_block_assets() {
-	
 	// Styles.
 	wp_enqueue_style(
 		'algori_pdf_viewer-cgb-style-css', // Handle.
@@ -44,21 +43,11 @@ add_action( 'enqueue_block_assets', 'algori_pdf_viewer_cgb_block_assets' );
  * @since 1.0.0
  */
 function algori_pdf_viewer_cgb_editor_assets() {
-	
-	// Localize path for Plugin Directory, so that I can acess it in JavaScript.
-	// $algori_pdf_viewer_plugin_directory_path = plugins_url();
-	// wp_localize_script( 'algori_pdf_viewer-cgb-block-path', 'algoriPDFViewerPluginDirectoryPath', $algori_pdf_viewer_plugin_directory_path );
-	
-	/*$js_src = plugins_url( 'src/block/display-php-value.js', dirname( __FILE__ ) ); // plugin_dir_url( __FILE__ ) . 'display-php-value.js';
-	wp_enqueue_script( 'fe-display-php-value', $js_src, [], '0.1.0' );
-	$my_php_value = 'Hello Everybody 2';
-	wp_localize_script( 'fe-display-php-value', 'feWPLocalizeScriptDemo', array( 'phpValueToDisplay' => $my_php_value ) );*/
-	
 	// Scripts.
 	wp_enqueue_script(
 		'algori_pdf_viewer-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
@@ -66,7 +55,7 @@ function algori_pdf_viewer_cgb_editor_assets() {
 	// Localize path for Plugin Directory, so that I can acess it in JavaScript attached to 'algori_pdf_viewer-cgb-block-js' above.
 	$algori_pdf_viewer_plugin_directory_path = plugins_url();
 	wp_localize_script( 'algori_pdf_viewer-cgb-block-js', 'algoriPDFViewerPluginDirectoryPath', $algori_pdf_viewer_plugin_directory_path );
-	
+
 	// Styles.
 	wp_enqueue_style(
 		'algori_pdf_viewer-cgb-block-editor-css', // Handle.
@@ -74,10 +63,7 @@ function algori_pdf_viewer_cgb_editor_assets() {
 		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
 	);
-	
 } // End function algori_pdf_viewer_cgb_editor_assets().
 
 // Hook: Editor assets.
 add_action( 'enqueue_block_editor_assets', 'algori_pdf_viewer_cgb_editor_assets' );
-
-
