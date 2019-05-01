ac1671fefbe442315d7fd3f592fb526c4dc4e9f3
KEVIN BAZIRA <kvnbzr@gmail.com>
2019-05-01 09:02:43 +0300

    Improved: Documentation
    Improved: Documentation to match WP standards

--- src/init.php
+++ src/init.php
@@ -16,8 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Enqueue Gutenberg block assets for both frontend + backend.
  *
- * `wp-blocks`: includes block type registration and related functions.
- *
+ * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
 function algori_pdf_viewer_cgb_block_assets() {
@@ -25,8 +24,8 @@ function algori_pdf_viewer_cgb_block_assets() {
 	wp_enqueue_style(
 		'algori_pdf_viewer-cgb-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
+		array( 'wp-editor' ) // Dependency to include the CSS after it.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
 	);
 } // End function algori_pdf_viewer_cgb_block_assets().
 
@@ -36,10 +35,10 @@ add_action( 'enqueue_block_assets', 'algori_pdf_viewer_cgb_block_assets' );
 /**
  * Enqueue Gutenberg block assets for backend editor.
  *
- * `wp-blocks`: includes block type registration and related functions.
- * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
- * `wp-i18n`: To internationalize the block's text.
- *
+ * @uses {wp-blocks} for block type registration & related functions.
+ * @uses {wp-element} for WP Element abstraction — structure of blocks.
+ * @uses {wp-i18n} to internationalize the block's text.
+ * @uses {wp-editor} for WP editor styles.
  * @since 1.0.0
  */
 function algori_pdf_viewer_cgb_editor_assets() {
@@ -47,9 +46,8 @@ function algori_pdf_viewer_cgb_editor_assets() {
 	wp_enqueue_script(
 		'algori_pdf_viewer-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
-		true // Enqueue the script in the footer.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ) // Dependencies, defined above.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ) // Version: File modification time.
 	);
 	
 	// Localize path for Plugin Directory, so that I can acess it in JavaScript attached to 'algori_pdf_viewer-cgb-block-js' above.
@@ -61,7 +59,7 @@ function algori_pdf_viewer_cgb_editor_assets() {
 		'algori_pdf_viewer-cgb-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
 		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
+		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
 	);
 } // End function algori_pdf_viewer_cgb_editor_assets().
 
