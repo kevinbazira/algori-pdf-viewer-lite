c4d88c97b0eaf6c3175e431a3a7361be1dcad1a9
Kevin Bazira <kvnbzr@gmail.com>
2020-08-29 11:05:02 +0000

    Replaced block category 'common' with 'media'

--- src/block/block.js
+++ src/block/block.js
@@ -81,6 +81,13 @@ const blockAttributes = {
 };
 
 
+/**
+ * Cater for block categories in older versions of WordPress i.e < WP 5.5
+ */
+const hasFormattingCategory = wp.blocks.getCategories().some( function( category ) {
+	return category.slug === 'common';
+} );
+
 
 /**
  * Register: aa Gutenberg Block.
@@ -104,7 +111,7 @@ registerBlockType( 'algori-pdf-viewer/block-algori-pdf-viewer', {
 	
 	icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm10 5.5h1v-3h-1v3z"/></svg>, // Block icon from Material Design Icons → https://material.io/tools/icons/
 	
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	category: hasFormattingCategory ? 'common' : 'media', // Block category — Group blocks together based on common traits E.g. text, media, design, widgets, embeds, reusable.
 	
 	keywords: [ // Block search keywords
 		__( 'portable document format' ), 
